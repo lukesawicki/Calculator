d9143a770c8a4ce7bd20998cf26bc7e2ced5f335
lukesawicki <lukaszsa1@gmail.com>
2020-02-09 13:33:31 +0100

    Simple corrections

--- src/main/java/pl/lukesawicki/calculator/CalculatorApplication.java
+++ src/main/java/pl/lukesawicki/calculator/CalculatorApplication.java
@@ -3,11 +3,18 @@ package pl.lukesawicki.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// proszę o wykonanie prostej funkcji, której zadaniem jest dodanie 2 liczb. Jedna z nich niech będzie pobierana z
+// hipotetycznego API dostępnego online (można wykorzystać np. https://www.random.org/clients/http/api/), źródło drugiej
+// może być dowolne (Math.random(), pobierana z hipotetycznej bazy danych itp). W zadaniu chodzi głównie o pokazanie
+// stylu i sposobu kodowania (dekompozycja kodu, podział funkcji itp). Można użyć mockowych źródeł danych, nie muszą
+// być zaimplementowane wszystkie przypadki brzegowe (ale warto je mieć z tyłu głowy). Kod ten będzie służył jako punkt
+// wyjścia podczas rozmowy rekrutacyjnej. Ważne też, żeby kod napisany był w sposób, jaki uważa Pan za najlepszy pod
+// względem designu i stylu.
+
 @SpringBootApplication
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
-
 }
--- src/main/java/pl/lukesawicki/calculator/application/supplier/RandomOrgClient.java
+++ src/main/java/pl/lukesawicki/calculator/application/supplier/RandomOrgClient.java
@@ -4,6 +4,7 @@ import com.google.common.io.CharStreams;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.nullness.Opt;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,11 +14,14 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Optional;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+
 @Slf4j
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 class RandomOrgClient {
     private final URL url;
+
     static RandomOrgClient newInstance() {
         try {
             return new RandomOrgClient(new URL("https://www.random.org/integers/?num=1&min=1&max=999&col=1&base=10&format=plain&rnd=new"));
@@ -28,29 +32,29 @@ class RandomOrgClient {
     }
 
     Number getNumber(){
+        return getRandomNumber();
+    }
+
+    private Number getRandomNumber() {
+        Number number = null;
         try {
-            return getRandomNumber();
-        } catch (IOException e) {
-            log.error("Boom", e);
-            throw new IllegalStateException(e);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+             number = parseHttpResponse(connection);
+            connection.disconnect();
+        } catch (ProtocolException exception) {
+            log.error("Error during parsing HTTP response", exception);
+        } catch (IOException exception) {
+            log.error("Error during parsing HTTP response", exception);
+        } catch (Exception exception) {
+            log.error("Error", exception);
         }
+        return number;
     }
 
-    private Number getRandomNumber() throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        if(200 != connection.getResponseCode()) {
-            throw new IllegalStateException("Response from random.org is not ok.");
-        }
-        Integer value = parseHttpResponse(connection);
-        connection.disconnect();
-        return value;
-    }
-
-    private int parseHttpResponse(HttpURLConnection connection) throws IOException {
+    private double parseHttpResponse(HttpURLConnection connection) throws IOException {
         try (final Reader reader = new InputStreamReader((InputStream) connection.getContent())) {
-            return Integer.valueOf(CharStreams.toString(reader).replaceAll("\\s",""));
+            return Double.parseDouble(CharStreams.toString(reader).replaceAll("\\s",""));
         }
     }
-
 }
